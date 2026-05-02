294279986c0d0e246f80169c48c9f8c13bb920b0
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2026-05-02 18:43:40 -0400

    ASR-011 escape unicode command controls (#82)

--- approver/Tests/AgentSecretApproverTests/ApprovalCommandRenderingTests.swift
+++ approver/Tests/AgentSecretApproverTests/ApprovalCommandRenderingTests.swift
@@ -53,6 +53,35 @@ final class ApprovalCommandRenderingTests: XCTestCase {
         XCTAssertFalse(viewModel.command.contains("\u{001F}"))
     }
 
+    func testCommandDisplayEscapesUnicodeSpoofingControls() throws {
+        let unassigned = try XCTUnwrap(Unicode.Scalar(0x0378))
+        let argv = [
+            "/bin/echo",
+            "safe\u{202E}txt",
+            "join\u{200D}er",
+            "line\u{2028}sep",
+            "para\u{2029}sep",
+            "private\u{E000}use",
+            "unassigned\(unassigned)scalar"
+        ]
+        let viewModel = Self.viewModel(command: argv)
+
+        XCTAssertEqual(
+            viewModel.command,
+            "'/bin/echo' $'safe\\u202Etxt' $'join\\u200Der' $'line\\u2028sep' " +
+                "$'para\\u2029sep' $'private\\uE000use' $'unassigned\\u0378scalar'"
+        )
+        XCTAssertTrue(viewModel.commandNeedsInspector)
+        XCTAssertTrue(viewModel.commandInspectionText.contains("argv[1]: $'safe\\u202Etxt'"))
+        XCTAssertTrue(viewModel.commandInspectionText.contains("argv[2]: $'join\\u200Der'"))
+        XCTAssertFalse(viewModel.command.contains("\u{202E}"))
+        XCTAssertFalse(viewModel.command.contains("\u{200D}"))
+        XCTAssertFalse(viewModel.command.contains("\u{2028}"))
+        XCTAssertFalse(viewModel.command.contains("\u{2029}"))
+        XCTAssertFalse(viewModel.command.contains("\u{E000}"))
+        XCTAssertFalse(viewModel.command.unicodeScalars.contains(unassigned))
+    }
+
     func testStructuredCommandInspectorPreservesArgvBoundaries() {
         let viewModel = Self.viewModel(command: [
             "/usr/bin/env",
